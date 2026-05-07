07b19a7428ee0afcad6f3ed5efd1905858b9e0d3
Deepika C <deepikac@Deepikas-MacBook-Air.local>
2026-05-07 14:18:11 +0530

    Added root route

--- server/server.js
+++ server/server.js
@@ -8,28 +8,8 @@ const app = express();
 app.use(cors({
   origin: "*"
 }));
+
 app.use(express.json());
-
-const PORT = process.env.PORT || 5001;
-
-let connectionPromise;
-
-const connectDB = () => {
-  if (!connectionPromise) {
-    connectionPromise = mongoose.connect(process.env.MONGO_URI);
-  }
-
-  return connectionPromise;
-};
-
-app.use(async (req, res, next) => {
-  try {
-    await connectDB();
-    next();
-  } catch (error) {
-    next(error);
-  }
-});
 
 // Routes
 const contactRoutes = require("./routes/contactRoutes");
@@ -40,15 +20,20 @@ app.use("/api/admin", adminRoutes);
 app.use("/api/contact", contactRoutes);
 app.use("/api/projects", projectRoutes);
 
-if (require.main === module) {
-  connectDB().then(() => {
+// Root route
+app.get("/", (req, res) => {
+  res.send("Backend is running 🚀");
+});
+
+const PORT = process.env.PORT || 5001;
+
+// Connect DB + Start server
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
     console.log("MongoDB Connected");
 
     app.listen(PORT, () => {
       console.log(`Server running on ${PORT}`);
     });
   })
-    .catch(err => console.log(err));
-}
-
-module.exports = app;
+  .catch(err => console.log(err));
